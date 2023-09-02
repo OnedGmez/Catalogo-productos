84438870f2b9a8ba2b263c194d48254c706c0055
OnedGmez <oned.gomez@unah.hn>
2023-09-01 20:15:01 -0600

    Lineas de importación de modulos externos

--- src/main.js
+++ src/main.js
@@ -6,8 +6,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+//Importación modulos externos instalados
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
+import axios from 'axios'
+
 const app = createApp(App)
 
+app.use(bootstrap)
+app.config.globalProperties.axios = axios;
 app.use(createPinia())
 app.use(router)
 
