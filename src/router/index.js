6f8c9ab6114aa5307b4e74b29f79e1da07b5b3f4
OnedGmez <oned.gomez@unah.hn>
2023-09-12 16:52:21 -0600

    rutas correctas con childrens

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import login from '../views/loginPage.vue'
 import principalPage from '../views/principalPage.vue'
 import shoppingCart from '../views/shoppingCart.vue'
+import modalProducts from '../components/modalProducts.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,9 +14,16 @@ const router = createRouter({
       alias: '/'
     },
     {
-      path: '/principal/:user/',
+      path: '/principal/',
       name: 'principal-Page',
-      component: principalPage
+      component: principalPage,
+      children:[
+        {
+          path: ':product',
+          name: 'modal-Products',
+          component: modalProducts,
+        }
+      ]
     },
     {
       path: '/shopping-cart/:user/:date/',
