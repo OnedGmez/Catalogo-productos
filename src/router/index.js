049a22e3435751b229d632ecaec09afb113def3c
OnedGmez <oned.gomez@unah.hn>
2023-09-13 17:25:01 -0600

    Mejora en las rutas de accesos

--- src/router/index.js
+++ src/router/index.js
@@ -1,42 +1,57 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import login from '../views/loginPage.vue'
-import principalPage from '../views/principalPage.vue'
-import shoppingCart from '../views/shoppingCart.vue'
-import modalProducts from '../components/modalProducts.vue'
-import modalAccount from '../components/modalAccount.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import login from "../views/loginPage.vue";
+import principalPage from "../views/principalPage.vue";
+import shoppingCart from "../views/shoppingCart.vue";
+import modalProducts from "../components/modalProducts.vue";
+import modalAccount from "../components/modalAccount.vue";
+import contentPP from "../components/contentPP.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/login',
-      name: 'login',
+      path: "/login",
+      name: "login",
       component: login,
-      alias: '/'
+      alias: "/",
     },
     {
-      path: '/principal/',
-      name: 'principal-Page',
+      path: "/principal/",
+      name: "principal-Page",
       component: principalPage,
+      children: [
+        {
+          path: "Products/:category",
+          name: "content-principal-page",
+          component: contentPP,
+          children: [
+            {
+              path: ":product",
+              name: "modal-Products",
+              component: modalProducts,
+            },
+            {
+              path: "Account/:user",
+              name: "account-config",
+              component: modalAccount,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      path: "/shopping-cart/:date/:user",
+      name: "shopping-Cart",
+      component: shoppingCart,
       children:[
         {
-          path: ':product',
-          name: 'modal-Products',
-          component: modalProducts,
-        },
-        {
-          path: 'Account/:user',
-          name: 'account-config',
+          path: "Account/",
+          name: "account-config-shopping",
           component: modalAccount,
         }
       ]
     },
-    {
-      path: '/shopping-cart/:user/:date/',
-      name: 'shopping-Cart',
-      component: shoppingCart
-    }
-  ]
-})
+  ],
+});
 
-export default router
+export default router;
